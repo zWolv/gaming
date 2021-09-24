b963190ea523bcc8284172f371e6b16610682da0
zWolv <57898164+zWolv@users.noreply.github.com>
2021-09-24 10:14:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Eenvoudige p5js starter
+
+Dit template is een eenvoudig startframe voor een p5js-canvas in een webpagina
+
+- Lay-out en stijl verander je in index.html en style.css
+- Inhoud van het p5js-programma verander je in script.js
+
+Om in GitPod de pagina te kunnen zien, typ dit in de "terminal": python3 -m http.server 3000
---
+++ script.js
@@ -0,0 +1,26 @@
+/**
+ * setup
+ * de code in deze functie wordt één keer uitgevoerd door
+ * de p5 library, zodra het spel geladen is in de browser
+ */
+function setup() {
+  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  createCanvas(1280, 720);
+
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+}
+
+
+/**
+ * draw
+ * de code in deze functie wordt meerdere keren per seconde
+ * uitgevoerd door de p5 library, nadat de setup functie klaar is
+ */
+function draw() {
+  // stel vulkleur in
+  fill(100, 100, 255);
+
+  // teken een cirkel
+  ellipse(50,50,80,80);
+}
